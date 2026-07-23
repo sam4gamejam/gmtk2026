92f29fa8e3a70e2d812437ec28deacf477360947
Guilherme Carvalho Ferrari <guicferrari@hotmail.com>
2026-07-22 22:57:14 -0300

    Add tool por pickable tile

--- scripts/pickable_tile.gd
+++ scripts/pickable_tile.gd
@@ -1,10 +1,23 @@
+@tool
 class_name PickableTile
 extends Node2D
+
+@export var tile_texture: Texture2D:
+	set(value):
+		tile_texture = value
+		_update_sprite()
 
 @onready var sprite: Sprite2D = $Sprite2D
 
 var source_id: int = -1
 var atlas_coords: Vector2i = Vector2i(-1, -1)
+
+func _ready() -> void:
+	_update_sprite()
+
+func _update_sprite() -> void:
+	if is_node_ready() and sprite:
+		sprite.texture = tile_texture
 
 func setup(texture: Texture2D, region: Rect2, p_source_id: int, p_atlas_coords: Vector2i) -> void:
 	sprite.texture = texture
